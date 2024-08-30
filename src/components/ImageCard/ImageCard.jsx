021dff7d160243027526a04ba042559568b6f780
ninaasieieva <160421199+ninaasieieva@users.noreply.github.com>
2024-08-30 19:03:42 +0300

    2

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,19 +1,42 @@
-import css from './ImageCard.module.css';
+import css from "./ImageCard.module.css"
+import { FiUser } from "react-icons/fi";
+import { FiThumbsUp } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import ImageModal from "../ImageModal/ImageModal"
 
-function ImageCard({ image, onSmallImgClick, onClick }) {
-  const handleClick = () => {
-    onSmallImgClick(image);
-    onClick();
-  };
-  return (
-    <div onClick={handleClick}>
-      <img
-        className={css.galleryImage}
-        src={image.urls.small}
-        alt={image.description}
-      />
-    </div>
-  );
+export default function ImageCard({ obj: { user: { first_name, instagram_username }, likes, tags, urls: { small, regular } } }, isOpen) {
+    const handleClick = (velue) => {
+        isOpen(velue);
+        console.log(velue);
+
+    };
+    return (
+
+        <div className={css.card}  >
+            <ImageModal >
+                <><img onClick={handleClick} className={css.imag} src={regular} alt={tags} /></>
+            </ImageModal>
+
+            <img onClick={handleClick} className={css.imag} src={small} alt={tags} />
+            {/* <img className={css.imag} src={small} alt={tags} /> */}
+
+            <div className={css.cardCommant}>
+                <div className={css.comItem}>
+                    <p className={css.commant}> <FiUser className={css.icon} /> {first_name}</p>
+                </div>
+                <div className={css.comIte}>
+                    <p className={css.commant}><FiThumbsUp className={css.icon} /> {likes}</p>
+                </div>
+                <div className={css.comIt}>
+                    <p className={css.commant}><FiInstagram className={css.icon} /> {instagram_username}</p>
+                </div>
+
+            </div>
+        </div>
+
+    );
 }
 
-export default ImageCard;
+// { <ImageModal onClick={() => setIsOpen(modalIsOpen + 1)} /> }
+// { <ImageModal overlayRef={node => (this.overlayRef = node)}
+//contentRef = { node => (this.contentRef = node)} onClick={() => setIsOpen(modalIsOpen + 1)} /> } 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,22 +1,99 @@
-import ImageCard from '../ImageCard/ImageCard';
 import css from './ImageGallery.module.css';
+import ImageCard from "../ImageCard/ImageCard"
 
-const ImageGallery = ({ images, onSmallImgClick, onClick }) => {
-  return (
-    <ul className={css.galleryList}>
-      {images.map((image, index) => {
-        return (
-          <li className={css.galleryItem} key={index}>
-            <ImageCard
-              onSmallImgClick={onSmallImgClick}
-              image={image}
-              onClick={onClick}
-            />
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+export default function ImageGallery({ items }) {
+    
+    return (
+        <main>
+            <div >
+                <ul className={css.list}>
+                    {items.map((task) => (
+                        <li className={css.item} key={task.id}>
+                            <ImageCard obj={task} ><a href={task.urls} target="_blank" rel="noreferrer noopener"> </a></ImageCard>
+                        </li>
+                    ))  
+                    }
+                    
+                </ul>
+            </div>
+        </main>
+    );
+}
 
-export default ImageGallery;
+// {
+//     tasks.map((task) => (
+//         <li className={css.item} key={task.id} >
+//             <Contact obj={task} onDelete={onDelete} />
+//         </li>
+//     ))
+// }
+
+// export default function ImageGallery({ items }) {
+//     return (
+//         <main>
+//             <div >
+
+//                 <ul className={css.list}>
+
+//                     {items.map(({ id, url, username }) => (
+//                         <li className={css.item} key={id}>
+//                             <ImageCard obj={use} />
+//                             <a href={url} target="_blank" rel="noreferrer noopener">
+//                                 {username}
+//                             </a>
+
+//                         </li>
+//                     ))}
+//                 </ul>
+
+//             </div>
+//         </main>
+//     );
+
+// };
+
+// export default function ImageGallery({ items }) {
+//     return (
+//         <main>
+//             <div >
+
+//                 <ul className={css.cardCommant}></ul>
+// const markup = images
+//     .map((image) =>
+//         ` 
+//     <li class="gallery-item">
+//     <a class="gallery-link" href="${image.largeImageURL}">
+//     <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" title="${image.name}"/></a>
+//     <div class="gallery-paragraf">
+//     <p class="gallery-commant">likes ${image.likes}</p>
+//     <p class="gallery-commant">views ${image.views}</p>
+//     <p class="gallery-commant">comments ${image.comments}</p>
+//     <p class="gallery-commant">downloads ${image.downloads}</p>
+//     </div>
+//     </li> 
+//      ` )
+//     .join("")
+// galleryContainer.insertAdjacentHTML("beforeend", markup);
+// lightbox.refresh();
+      
+//   };
+
+// className = { css.container }
+
+// const markup = images
+//     .map((image) =>
+//         `
+//     <li class="gallery-item">
+//     <a class="gallery-link" href="${image.largeImageURL}">
+//     <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" title="${image.name}"/></a>
+//     <div class="gallery-paragraf">
+//     <p class="gallery-commant">likes ${image.likes}</p>
+//     <p class="gallery-commant">views ${image.views}</p>
+//     <p class="gallery-commant">comments ${image.comments}</p>
+//     <p class="gallery-commant">downloads ${image.downloads}</p>
+//     </div>
+//     </li>
+//      ` )
+//     .join("")
+// galleryContainer.insertAdjacentHTML("beforeend", markup);
+// lightbox.refresh();
